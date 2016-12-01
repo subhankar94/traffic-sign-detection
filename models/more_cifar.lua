187472c39d725ce45b300175db1dd88e4c409312
Subhankar Ghosh <subhankar94@gmail.com>
2016-12-01 16:47:52 -0500

    updating model to deal with gpu

--- models/more_cifar.lua
+++ models/more_cifar.lua
@@ -1,5 +1,5 @@
 local nn = require 'nn'
-
+local image = require 'image'
 
 local Convolution = nn.SpatialConvolution
 local Tanh = nn.Tanh
@@ -7,15 +7,19 @@ local ReLU = nn.ReLU
 local Max = nn.SpatialMaxPooling
 local View = nn.View
 local Linear = nn.Linear
+local LCN = nn.SpatialContrastiveNormalization
 
 local model  = nn.Sequential()
 
-model:add(Convolution(3, 16, 5, 5))
+model:add(Convolution(3, 16, 5, 5, 1, 1, 2, 2))
 model:add(ReLU())
 model:add(Convolution(16, 64, 5, 5))
 model:add(ReLU())
+--model:add(LCN(64, image.gaussian(3)))
 model:add(Max(2,2,2,2))
 model:add(Convolution(64, 128, 5, 5))
+model:add(ReLU())
+model:add(Convolution(128, 128, 3, 3))
 model:add(ReLU())
 model:add(Max(2,2,2,2))
 model:add(View(2048))
