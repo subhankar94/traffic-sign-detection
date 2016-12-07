98757d397a4480c82bc5f95e975ac119f074a4d9
Subhankar Ghosh <subhankar94@gmail.com>
2016-12-07 07:20:05 -0500

    rerun using deper network

--- models/more_cifar.lua
+++ models/more_cifar.lua
@@ -13,23 +13,18 @@ local model = nn.Sequential()
 
 model:add(Convolution(3,  32, 5, 5)) -- 16*28*28
 model:add(ReLU())
-model:add(Convolution(32, 128, 3, 3)) -- 128*26*26
+model:add(Convolution(32, 64, 3, 3)) -- 64*26*26
 model:add(ReLU())
-model:add(Max(3,3,1,1)) -- 128*24*24
-model:add(Convolution(128, 256, 5, 5)) -- 256*20*20
+model:add(Max(3, 3, 1, 1)) -- 64*24*24
+model:add(Convolution(64, 128, 5, 5)) -- 128*20*20
 model:add(ReLU())
-model:add(Convolution(256, 512, 3, 3)) -- 512*18*18
+model:add(Convolution(128, 256, 5, 5)) -- 256*16*16
 model:add(ReLU())
-model:add(Convolution(512, 1024, 3, 3)) -- 1024*16*16
-model:add(ReLU())
-model:add(Max(2,2,2,2)) -- 1024*8*8
-model:add(Convolution(1024, 2048, 5, 5)) -- 2048*4*4
-model:add(ReLU())
-model:add(Convolution(2048, 4096, 3, 3)) -- 4096*2*2
-model:add(ReLU())
-model:add(Max(2,2,2,2)) -- 4096*1*1
-model:add(View(4096))
+model:add(Max(2,2,2,2)) -- 256*8*8
+model:add(View(16384))
 model:add(Dropout(0.30))
+model:add(Linear(16384, 4096))
+model:add(ReLU())
 model:add(Linear(4096, 64))
 model:add(ReLU())
 model:add(Linear(64, 43))
