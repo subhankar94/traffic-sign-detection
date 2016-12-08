367e18998083fabfe0a9f2a19e746ff2b14bb516
Subhankar Ghosh <subhankar94@gmail.com>
2016-12-07 20:47:19 -0500

    cleaning up

--- utils.lua
+++ utils.lua
@@ -7,18 +7,6 @@ function resize(img)
     return image.scale(img, WIDTH, HEIGHT)
 end
 
-function shift(img)
-    return image.translate(img, torch.random(-5,5), torch.random(-5,5)) 
-end
-
-function rotate(img)
-    return image.rotate(img, torch.random(-0.3926990816, 0.3926990816)) --pi/8
-end
-
-function scale(img)
-    return image.scale(img, WIDTH+torch.random(-5,5), HEIGHT+torch.random(-5,5))
-end
-
 --[[
 -- Hint:  Should we add some more transforms? shifting, scaling?
 -- Should all images be of size 32x32?  Are we losing 
@@ -26,18 +14,8 @@ end
 --]]
 function transformInput(inp)
     f = tnt.transform.compose{
-        --[1] = shift,
-        --[2] = rotate,
-        --[3] = scale,
         [1] = function(x) return image.rgb2yuv(x) end,
         [2] = resize
-    }
-    return f(inp)
-end
-
-function transformTestInput(inp)
-    f = tnt.transform.compose{
-        [1] = resize
     }
     return f(inp)
 end
@@ -56,7 +34,7 @@ end
 function getTestSample(dataset, idx)
     r = dataset[idx]
     file = "./data/test_images/" .. string.format("%05d.ppm", r[1])
-    return transformTestInput(image.load(file))
+    return transformInput(image.load(file))
 end
 
 
