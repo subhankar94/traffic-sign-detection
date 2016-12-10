a394ad829d46f4b8c0b948da2a96822a03f60657
Subhankar Ghosh <subhankar94@gmail.com>
2016-12-10 16:27:01 -0500

    shallower, wider model

--- models/more_cifar.lua
+++ models/more_cifar.lua
@@ -12,20 +12,18 @@ local Dropout = nn.Dropout
 local model = nn.Sequential()
 
 -- input 3*48*48
-model:add(Convolution(3,  64, 7, 7)) -- 64*42*42
+model:add(Convolution(3,  128, 5, 5)) -- 128*44*44
 model:add(ReLU())
-model:add(Convolution(64, 128, 5, 5, 1, 1, 1)) -- 128*40*40
+model:add(Max(2, 2, 2, 2)) -- 128*22*22
+model:add(Convolution(128, 256, 3, 3)) -- 256*20*20
 model:add(ReLU())
-model:add(Max(2, 2, 2, 2)) -- 128*20*20
-model:add(Convolution(128, 256, 5, 5)) -- 256*16*16
+model:add(Max(2, 2, 2, 2)) -- 256*10*10
+model:add(Convolution(256, 512, 5, 5)) -- 256*6*6
 model:add(ReLU())
-model:add(Max(2, 2, 2, 2)) -- 256*8*8
-model:add(Convolution(256, 384, 5, 5)) -- 384*4*4
-model:add(ReLU())
-model:add(Max(2,2,2,2)) -- 384*2*2
-model:add(View(1536))
+model:add(Max(2, 2, 2, 2)) -- 512*3*3
+model:add(View(4608))
 model:add(Dropout(0.40))
-model:add(Linear(1536, 100))
+model:add(Linear(4608, 100))
 model:add(ReLU())
 model:add(Linear(100, 43))
 
